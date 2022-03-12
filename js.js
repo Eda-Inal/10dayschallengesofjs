c034b30dbce74f1f65d995e3e37305516bac3d41
Eda İnal <83394945+Eda-Inal@users.noreply.github.com>
2022-03-12 09:55:03 +0300

    Update js.js

--- js.js
+++ js.js
@@ -13,3 +13,20 @@ function getPerimeter(length, width) {
   perimeter = 2 * (length + width);
   return perimeter;
 }
+/*****************Day 3: Intro to Conditional Statements*****************/
+
+function main() {
+    const N = parseInt(readLine().trim(), 10);
+     if (N % 2 ==1){
+         console.log("Weird");
+     }
+     if (N % 2 ==0 && N >= 2 && N <= 5){
+         console.log("Not Weird");
+     }
+     if (N % 2 ==0 && N >= 6 && N <= 20){
+         console.log("Weird");
+     }
+     if (N % 2 ==0 && N > 20){
+         console.log("Not Weird");
+     }
+}
