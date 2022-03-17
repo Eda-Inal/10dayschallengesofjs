e8515e5944165371aeb4bb913073df0d78ff200b
Eda İnal <83394945+Eda-Inal@users.noreply.github.com>
2022-03-17 11:09:29 +0300

    Update js.js

--- js.js
+++ js.js
@@ -53,3 +53,10 @@ function main() {
     
     return letter;
 }
+/*********** day1 functions**********/
+function factorial(n) {
+  if (n < 2) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
